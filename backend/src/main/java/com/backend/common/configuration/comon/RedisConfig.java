c357b5eea574b5d604a4286d891677953b47b4e1
Yohan <sprae114@naver.com>
2025-02-07 23:02:17 +0900

    feat : 레디스 설정 구현

---
+++ backend/src/main/java/com/backend/common/configuration/comon/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.backend.common.configuration.comon;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * 레디스 설정 파일
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
