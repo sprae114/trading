7648349c4a8885baec661a4604e0b7a2e80fcfc5
Yohan <sprae114@naver.com>
2025-02-19 08:12:26 +0900

    Entity 범위지정

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -2,11 +2,22 @@ package com.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EntityScan(basePackages =
+		{
+				"com.backend.user.model.entity" ,
+				"com.backend.post.model.entity"
+		}
+)
+@EnableJpaRepositories(basePackages = {
+		"com.backend.user.repository" ,
+		"com.backend.post.repository"
+})
 public class BackendApplication {
 
 	public static void main(String[] args) {
