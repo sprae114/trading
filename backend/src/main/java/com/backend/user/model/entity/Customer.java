af3454564f3085a72b5e9e1a7e3960d0323ed69c
Yohan <sprae114@naver.com>
2025-03-11 15:33:12 +0900

    feat : login customer 유효성 검사 추가

--- backend/src/main/java/com/backend/user/model/entity/Customer.java
+++ backend/src/main/java/com/backend/user/model/entity/Customer.java
@@ -3,6 +3,7 @@ package com.backend.user.model.entity;
 import com.backend.common.model.BaseEntity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.*;
 
 @Builder(toBuilder = true)
@@ -18,10 +19,14 @@ public class Customer extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Email
+    @Column(unique = true, nullable = false)
     private String email;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // 클라이언트는 비번 조회 불가능
+    @Column(nullable = false)
     private String pwd;
 
+    @Column(nullable = false)
     private String name;
 }
--- backend/src/main/java/com/backend/user/repository/CustomerRepository.java
+++ backend/src/main/java/com/backend/user/repository/CustomerRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Optional<Customer> findByEmail(String email);
+    Optional<Customer> findByName(String name);
     long countByEmail(String email);
 
 }
