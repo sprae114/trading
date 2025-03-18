37a52a8c3e33ddc98c1213b704f6236807d206be
Yohan <sprae114@naver.com>
2025-03-18 19:48:13 +0900

    feat : like 엔티티에 index 추가

--- backend/src/main/java/com/backend/post/model/entity/Likes.java
+++ backend/src/main/java/com/backend/post/model/entity/Likes.java
@@ -1,19 +1,28 @@
 package com.backend.post.model.entity;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder
 @Getter
 @Entity
+@Table(
+        name = "likes",
+        indexes = {
+                @Index(name = "idx_post_id_customer_id", columnList = "postId, customerId"), // 복합 인덱스
+                @Index(name = "idx_customer_id", columnList = "customerId") // 단일 인덱스
+        }
+)
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class) // @CreatedDate를 위해 추가
 public class Likes {
 
     @Id
