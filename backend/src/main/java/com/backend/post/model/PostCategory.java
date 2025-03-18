3752ac9cabcac0f0fe62066c3a2a588f7d46e238
Yohan <sprae114@naver.com>
2025-03-19 04:27:55 +0900

    update : post enum 수정

--- backend/src/main/java/com/backend/post/model/PostCategory.java
+++ backend/src/main/java/com/backend/post/model/PostCategory.java
@@ -1,10 +1,14 @@
 package com.backend.post.model;
 
 public enum PostCategory {
-    FOOD, // 음식
-    DAILY, // 일상
-    TRAVEL, // 여행
-    FASHION, // 패션
-    ANIMAL, // 동물
-    ;
+    ELECTRONICS,       // 전자제품
+    FASHION,          // 패션
+    HOME_APPLIANCES,  // 가전/가구
+    BOOKS,            // 도서
+    SPORTS,           // 스포츠/레저
+    BABY,            // 유아동
+    BEAUTY,          // 뷰티
+    VEHICLES,        // 차량/오토바이
+    HOBBY,           // 취미/게임
+    GENERAL          // 기타
 }
