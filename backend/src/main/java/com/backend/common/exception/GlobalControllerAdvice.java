4c8686af68ad471f5e6d81950678e6d44d6dfdaf
Yohan <sprae114@naver.com>
2025-04-10 23:57:11 +0900

    update : jwt 정보 불러오기 오류
    jwt 불러올때, 제대로 읽지 못하는 오류 발생. 전처리 이후 jwt 읽도록 수정

--- backend/src/main/java/com/backend/common/exception/GlobalControllerAdvice.java
+++ backend/src/main/java/com/backend/common/exception/GlobalControllerAdvice.java
@@ -2,7 +2,9 @@ package com.backend.common.exception;
 
 import com.backend.common.exception.dto.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,4 +22,14 @@ public class GlobalControllerAdvice {
                 .body(Response.error(e.getErrorCode().name()));
 
     }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> badCredentialsHandler(BadCredentialsException e) {
+        log.error("Error occurs {}", e.toString());
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(Response.error(e.getMessage()));
+    }
+
 }
--- backend/src/main/java/com/backend/common/filter/JWTTokenValidatorFilter.java
+++ backend/src/main/java/com/backend/common/filter/JWTTokenValidatorFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -26,12 +27,19 @@ import java.util.List;
 /**
  * JWT 토큰 검증 필터
  */
+@Slf4j
 public class JWTTokenValidatorFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        String jwt = request.getHeader(ApplicationConstants.JWT_HEADER);
+        log.info("JWTTokenValidatorFilter doFilterInternal");
+        String authHeader = request.getHeader(ApplicationConstants.JWT_HEADER); // "Authorization"
+
+        String jwt = null;
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            jwt = authHeader.substring(7).trim(); // "Bearer " 제거 후 공백 정리
+        }
 
         if(null != jwt) {
             try {
@@ -41,19 +49,22 @@ public class JWTTokenValidatorFilter extends OncePerRequestFilter {
                     // JWT 비밀키 설정
                     String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY,
                             ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
+
                     SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+                    log.debug("JWTTokenValidatorFilter secret: {}, key: {}", secret, secretKey);
 
                     if(null !=secretKey) {
                         // JWT 토큰 검증
                         Claims claims = Jwts.parser().verifyWith(secretKey)
                                 .build().parseSignedClaims(jwt).getPayload();
-                        String username = String.valueOf(claims.get("username"));
+                        String email = String.valueOf(claims.get("email"));
                         String authorities = String.valueOf(claims.get("authorities"));
                         List<GrantedAuthority> authorityList = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
 
                         // 인증 객체 생성
-                        Authentication authentication = new UsernamePasswordAuthenticationToken(username,
+                        Authentication authentication = new UsernamePasswordAuthenticationToken(email,
                                 null, authorityList);
+                        log.info("JWTTokenValidatorFilter doFilterInternal: email: {} authorities: {}", authentication.getDetails() ,authentication.getAuthorities());
 
                         // 인증 객체를 SecurityContextHolder에 저장
                         SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -61,7 +72,8 @@ public class JWTTokenValidatorFilter extends OncePerRequestFilter {
                 }
 
             } catch (Exception exception) {
-                throw new BadCredentialsException(ErrorCode.INVALID_TOKEN.getMessage());
+                log.error("JWTTokenValidatorFilter doFilterInternal", exception);
+                throw new BadCredentialsException(ErrorCode.INVALID_TOKEN.getMessage(), exception);
             }
         }
         filterChain.doFilter(request,response);
