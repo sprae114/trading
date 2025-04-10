26ce863d010bd4ea3b65c886628de7e9afde89d4
Yohan <sprae114@naver.com>
2025-04-11 00:39:56 +0900

    feat : 전역 변수 설정

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -5,9 +5,13 @@ const store = createStore({
         return {
             backendUrl: 'http://localhost:8080',
 
+            //네이게이션바 관련
+            isDrawerOpen: false,
+
             // 로그인 관련
             isLoggedIn : false,
-            isOTPVaild: false,
+            isOTPValid: false,
+            customerInfo: null,
             
             // 모달 관련
             isModalOpen: false,
@@ -19,9 +23,17 @@ const store = createStore({
             isBtnAlertVisible: false,
             alertType: '',
             alertMessage: '',
+
+            // 상품
+            showProduct : {},
         }
     },
     mutations :{
+        // 네비게이션바 관련
+        TOGGLE_DRAWER(state) {
+            state.isDrawerOpen = !state.isDrawerOpen;
+        },
+        
         // 모달 관련
         SET_MODAL_OPEN(state, payload) {
             state.isModalOpen = payload
@@ -37,16 +49,23 @@ const store = createStore({
         },
         
         SHOW_NOMAL_ALERT_MESSAGE(state, [payload1, payload2]) {
-            state.alertTpye = payload1;
+            state.alertType = payload1;
             state.alertMessage = payload2;
         
+           // 기존 타이머가 있으면 지우기
+            if (state.alertTimeout) {
+                clearTimeout(state.alertTimeout);
+            }
+
+            // 알람 열기
             state.isAlertOpen = true;
-        
-            setTimeout(() => {
+
+            // 새 타이머 설정
+            state.alertTimeout = setTimeout(() => {
                 state.isAlertOpen = false;
+                state.alertTimeout = null; // 타이머 종료 후 초기화
             }, 2000);
         },
-        
         
         SET_NOMAL_ALERT(state, payload) {
             state.isAlertOpen = payload;
@@ -58,17 +77,38 @@ const store = createStore({
         
         // OTP 관련
         SET_OTP_VAILD(state, payload) {
-            state.isOTPVaild = payload;
+            state.isOTPValid = payload;
         },
         
         SET_IS_LOIGIN(state, payload) {
             state.isLoggedIn = payload;
         },
-    },
-        actions : {
-            // ajax 요청하는 곳
+
+        GET_CUSTOMER_INFO(state, payload) {
+            const storedData = sessionStorage.getItem('userInformation');
             
-    }
+            if (storedData) {
+                const parsedData = JSON.parse(storedData);
+                console.log(parsedData.customer);
+                state.customerInfo = parsedData.customer;
+            }
+            else {
+                state.customerInfo = null;
+                this.$store.commit('SET_MODAL_OPEN', true);
+                this.$store.commit('SET_MODAL_MESSAGE', '로그인이 필요한 서비스입니다.');
+            }
+        },
+
+        // 상품
+        SET_SHOWPRODUCT(state, payload) {
+            state.showProduct = payload;
+        }
+    },
+    getters: {
+        getShowProduct(state) {
+            return state.showProduct;
+        },
+    },
 })
 
 export default store
